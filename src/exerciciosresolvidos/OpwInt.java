e7fe123893d6e434d83c7f4ec38ad9800e6cd900
samuel-marinho <samuelmarinho.dev@gmail.com>
2023-10-12 10:30:14 -0300

    Exercise 1 - Operations with Int numbers

---
+++ src/exerciciosresolvidos/OpwInt.java
@@ -0,0 +1,16 @@
+package exerciciosresolvidos;
+
+import java.util.Scanner;
+
+public class OpwInt {
+    public static void main(String[] args) {
+        /* Operações com valores inteiros */;
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int resultado = x+y;
+
+        System.out.printf("A soma entre %d e %d é %d", x, y, resultado);
+    }
+}
